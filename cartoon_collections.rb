cf8b9969ba7962d2a19747a03811f0e8d2feb200
Lisa Walters <campi_l@fortlewis.edu>
2020-08-26 22:20:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,9 @@ def long_planeteer_calls(array)
 array.any? {|call|
   call.length > 4
 }
-
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array) 
+  array.include("cheddar", "gouda", "camembert")
+
 end
